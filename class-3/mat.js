27a9a389831ce999c349a17c23df5cf61570819e
Henrique Souza <henrique.asouza@outlook.com>
2021-10-19 22:04:42 -0300

    chore: add class-3

---
+++ class-3/mat.js
@@ -0,0 +1,8 @@
+var num = 10
+var num2 = 20
+
+function sum(a, b) {
+    return a + b
+}
+
+console.log(sum(num, num2))
---
+++ class-3/nome.js
@@ -0,0 +1,4 @@
+var name = 'Henrique'
+var subname = 'Souza'
+
+console.log('Nome: ' + name + ' ' + subname)
