0d0186ea57616c8bae3a591db4ee345565f38f6b
Henrique Souza <henrique.asouza@outlook.com>
2021-10-19 23:40:21 -0300

    chore: add class-10

---
+++ class-10/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/html/about.html')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Welcome to my blog!')
+})
+
+app.get('/ola/:cargo/:name/:color', (req, res) => {
+    res.send(`<h1>Hi ${req.params.name}</h1><h2>Your job is ${req.params.cargo}</h2><h2>Your favorite color is ${req.params.color}</h2>`)
+})
+
+app.listen(8080, () => {
+    console.log('Server running on http://localhost:8080')
+})
