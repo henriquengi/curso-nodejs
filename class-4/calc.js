2783e1de420b5adac23f2e4083851652605e3cc9
Henrique Souza <henrique.asouza@outlook.com>
2021-10-19 22:14:32 -0300

    chore: add class-4

---
+++ class-4/calc.js
@@ -0,0 +1,9 @@
+const somaFunc = require('./sum')
+const subFunc = require('./subtr')
+const multiFunc = require('./multi')
+const divFunc = require('./div')
+
+console.log(somaFunc(10,2))
+console.log(subFunc(10,2))
+console.log(multiFunc(10,2))
+console.log(divFunc(10,2))
