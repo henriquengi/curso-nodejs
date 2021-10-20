e076255a634d7ed9d4f7d673152f85e4ed5f0111
Henrique Souza NGI <henrique.souza@ngi.com.br>
2021-10-20 14:42:05 -0300

    chore: add class-26

---
+++ class-26/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const Post = require('./models/Post')
+
+app.set('views', __dirname + '/views');
+
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main', 
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    Post.findAll({
+        order: [['id', 'DESC']]
+    }).then((dbpost) => {
+        res.render('home', { posts: dbpost })
+    })
+
+})
+
+app.get('/cad', (req, res) => {
+    res.render('form')
+})
+
+app.post('/addpost', (req, res) => {
+
+    Post.create({
+        title: req.body.title,
+        content: req.body.content
+    }).then(() => {
+        res.redirect('/')
+    }).catch((error) => {
+        res.send('Error - Coudnt create post!<br>' + error)
+    })
+})
+
+app.get('/delet/:id', (req, res) => {
+    Post.destroy({
+        where: {'id': req.params.id}
+    }).then(() => {
+        res.send('Post deleted!')
+    }).catch((error) => {
+        res.send('This post doesnt exist')
+    })
+})
+
+app.listen(8080, () => {
+    console.log('Server running at http://localhost:8080')
+})
